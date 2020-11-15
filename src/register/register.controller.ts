901055afe3504778d3b6b04bf3ecb0724ebe4954
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2020-11-15 00:36:11 -0400

    Finalizado integração com o dialogflow

--- src/register/register.controller.ts
+++ src/register/register.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateConversationDto } from './dto/create-conversation.dto';
 import { RegisterService } from './register.service';
 
 @Controller('/api/register')
 export class RegisterController {
   constructor(private readonly registerService: RegisterService) { }
 
-  @Get()
+  @Get('/welcome')
   async welcome (): Promise<any> {
     return await this.registerService.welcome();
+  }
+
+  @Post('/conversation')
+  async conversation (
+    @Body() createConversationDto: CreateConversationDto,
+  ): Promise<any> {
+    return await this.registerService.conversation(createConversationDto);
   }
 }
--- src/register/register.service.ts
+++ src/register/register.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import * as dialogflow from 'dialogflow';
 import * as uuid from 'uuid';
+import { CreateConversationDto } from './dto/create-conversation.dto';
 
 @Injectable()
 export class RegisterService {
   async welcome (): Promise<any> {
-    const sessionId = uuid.v4();
-    const sessionClient = new dialogflow.SessionsClient();
-    const sessionPath = sessionClient.sessionPath('newagent-nogv', sessionId);
+    const sessionClient = await this.createSessionClient();
+    const sessionPath = await this.createSessionPath(sessionClient);
 
     const request = {
       session: sessionPath,
@@ -25,5 +25,45 @@ export class RegisterService {
       text: responses[0].queryResult.fulfillmentText,
       buffer: responses[0].outputAudio,
     };
+  }
+
+  async conversation (
+    createConversationDto: CreateConversationDto,
+  ): Promise<any> {
+    const sessionClient = await this.createSessionClient();
+    const sessionPath = await this.createSessionPath(sessionClient);
+
+    const { text, context } = createConversationDto;
+
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text,
+          languageCode: 'pt-BR',
+        },
+      },
+      queryParams: {
+        contexts: context,
+      },
+    };
+
+    const responses = await sessionClient.detectIntent(request);
+
+    return {
+      text: responses[0].queryResult.fulfillmentText,
+      context: responses[0].queryResult.outputContexts,
+      buffer: responses[0].outputAudio,
+    };
+  }
+
+  private async createSessionClient (): Promise<any> {
+    return Promise.resolve(new dialogflow.SessionsClient());
+  }
+
+  private async createSessionPath (sessionClient: any): Promise<any> {
+    const sessionId = uuid.v4();
+    const sessionPath = sessionClient.sessionPath('newagent-nogv', sessionId);
+    return Promise.resolve(sessionPath);
   }
 }
